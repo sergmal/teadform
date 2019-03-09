3c43e2ab386651012854563eb0528d22e1665004
Sergei Malõshev <serge@DESKTOP-OEB5RV0>
2019-03-09 20:50:26 +0200

    test

--- lab1/lab1/lab1.py
+++ lab1/lab1/lab1.py
@@ -1,1 +1,20 @@
-.
+#install spacy
+#install wikipedia
+
+import spacy
+nlp = spacy.load('en_core_web_sm')
+from spacy import displacy
+import wikipedia
+
+text = (u'Estonia is a country in Northern Europe. It is bordered to the north by the Gulf of Finland with Finland on the other side, to the west by the Baltic Sea with Sweden on the other side')
+
+doc = nlp(text)
+
+# Find named entities, phrases and concepts
+for entity in doc.ents:
+    print(entity.text, entity.label_)
+    wiki = wikipedia.page(entity.text)
+    print(wiki.url)
+
+#ava oma browseris localhost:5000
+displacy.serve(doc, style='ent')
